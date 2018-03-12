3571a0484fcbb2c9813da0d26d152801f8e0d8eb
markeastman <mark@eastmans.me.uk>
2018-03-12 23:41:02 +0000

    Update env-init.sh

--- my-second-scenario/env-init.sh
+++ my-second-scenario/env-init.sh
@@ -1,1 +1,3 @@
-git clone https://github.com/markeastman/katacoda-scenarios/my-first-scenario
+#git clone https://github.com/markeastman/katacoda-scenarios/my-first-scenario
+docker build -t webserver-image:v1
+docker run -d -p 80:80 webserver-image:v1
